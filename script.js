fc1a463dbf37d6a359240e6affe47be4d266dcac
kubby159 <83577397+kubby159@users.noreply.github.com>
2022-07-25 15:10:24 +0900

    todolist add function

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
-;(function () {
-  'use strict'
+(function () {
+  "use strict";
 
   const get = (target) => {
-    return document.querySelector(target)
-  }
+    return document.querySelector(target);
+  };
+
+  const API_URL = "http://localhost:3001/todos";
+  const $todos = get(".todos");
+  const $form = get(".todo_form");
+  const $todoInput = get(".todo_input");
 
   const createTodoElement = (item) => {
-    const { id, content } = item
-    const $todoItem = document.createElement('div')
-    $todoItem.classList.add('item')
-    $todoItem.dataset.id = id
+    const { id, content } = item;
+    const $todoItem = document.createElement("div");
+    $todoItem.classList.add("item");
+    $todoItem.dataset.id = id;
     $todoItem.innerHTML = `
             <div class="content">
               <input
@@ -35,10 +40,55 @@
                 <i class="fas fa-times"></i>
               </button>
             </div>
-      `
-    return $todoItem
-  }
+      `;
+    return $todoItem;
+  };
 
-  const init = () => {}
-  init()
-})()
+  //! renderAllTodos
+
+  const renderAllTodos = (todos) => {
+    $todos.innerHTML = ""; //초기화
+    todos.forEach((item) => {
+      const todoElement = createTodoElement(item);
+      $todos.appendChild(todoElement);
+    });
+  };
+
+  //! getTodos
+  const getTodos = () => {
+    fetch(API_URL)
+      .then((res) => res.json())
+      .then((todos) => renderAllTodos(todos))
+      .catch((error) => console.error(error));
+  };
+
+  // ! addTodo
+
+  const addTodo = (e) => {
+    e.preventDefault();
+    // fetch 이용해서 input value 전송하기
+    const todo = {
+      content: $todoInput.value,
+      completed: false,
+    };
+    fetch(API_URL, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(todo),
+    })
+      .then(getTodos)
+      .then(() => {
+        $todoInput.value = "";
+        $todoInput.focus();
+      })
+      .catch((error) => console.error(error));
+  };
+
+  const init = () => {
+    window.addEventListener("DOMContentLoaded", () => {
+      getTodos();
+    });
+    $form.addEventListener("submit", addTodo);
+  };
+  init();
+})();
